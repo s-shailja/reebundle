0b05db74e7450785790337cbefd95abf769a90ac
s-shailja <shailjasah12@gmail.com>
2025-05-20 02:59:13 -0700

    add github actions, tests, and pypi release test

---
+++ tests/rg_create_tests.py
@@ -0,0 +1,23 @@
+import pytest
+import numpy as np
+import nibabel as nib
+import networkx as nx
+import matplotlib.pyplot as plt
+import reebundle.construct as rc
+from mpl_toolkits.mplot3d import Axes3D
+import reebundle.visualization as vis
+import reebundle.distance as sn
+
+def test_rg_nodes_edges():
+    eps = 2.5
+    alpha = 3
+    delta = 5
+    trkpathI = "tests/data/"
+    file = "CA.trk"
+    streamlines = nib.streamlines.load(trkpathI+file).streamlines
+    h, node_loc = rc.constructRobustReeb(streamlines, eps, alpha, delta)
+    assert isinstance(h, nx.Graph)
+    assert isinstance(node_loc, dict)
+    assert len(h.nodes) == 9
+    assert len(h.edges) == 8
+    
